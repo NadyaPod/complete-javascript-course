a4ed69443dcdeaa1156385b48214d8cf7dd3c845
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-13 12:35:30 +0700

    complete lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,10 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is Fun!');
-
 console.log(40 + 8 + 23 - 10);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+let jonas_matilda = "jm";
+let $fuction = 27;
+let PI = 3.1415;
   
