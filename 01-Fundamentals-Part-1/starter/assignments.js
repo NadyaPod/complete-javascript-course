125e69325ed4f035f281a5819a36b862ba51026a
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-14 12:38:31 +0700

    complete lesson

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+let country = "Portugal";
+let continent = "Europe";
+let population = 10;
+let isIsland = false;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,26 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = 'Jonas';
-console.log(firstName);
+// let firstName = 'Jonas';
+// console.log(firstName);
 
-let jonas_matilda = "jm";
-let $fuction = 27;
-let PI = 3.1415;
+// let jonas_matilda = "jm";
+// let $fuction = 27;
+// let PI = 3.1415;
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
   
+// javaScriptIsFun = 'yes';
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year)
+// console.log(typeof year);
+
+// year = 1990;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
