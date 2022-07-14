7f829a57e83683c05424a0207d9b83000c5600ce
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-14 13:37:11 +0700

    complete cc#1

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,11 +1,33 @@
-let country = "Portugal";
-let continent = "Europe";
+const country = "Portugal";
+const continent = "Europe";
 let population = 10;
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = 'french';
 
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(isIsland);
-console.log(language);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+
+// const halfNumber = population / 2;
+// console.log(population++);
+
+// const finnlandPopulation = 6;
+// console.log(population > finnlandPopulation);
+// console.log(population < 33);
+
+// const description = country + ' is in '  + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description);
+
+const markHeight = 1.88;
+const johnHeight = 1.76;
+const markWeight = 95;
+const johnWeight = 85;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
