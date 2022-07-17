0e6778dc923b51425e7135e2584a292f4ee3bc8a
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-17 17:59:53 +0700

    complete cc#4

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -80,27 +80,34 @@ const isIsland = false;
 //   console.log('No winner');
 // }
 
-const language = 'finnish';
+// const language = 'finnish';
 
-switch (language) {
-  case 'chinese':
-  case 'mandarin': 
-    console.log('MOST number of native speakers!');
-    break;
-  case 'spanish':
-    console.log('2nd place in number of native speakers');
-    break;
-  case 'english':
-    console.log('3rd place');
-    break;
-  case 'hindi':
-    console.log('Number 4');
-    break;  
-  case 'arabic':
-    console.log('5th most spoken language');
-    break;  
-  default:
-    console.log('Great language too :D');
-}
+// switch (language) {
+//   case 'chinese':
+//   case 'mandarin': 
+//     console.log('MOST number of native speakers!');
+//     break;
+//   case 'spanish':
+//     console.log('2nd place in number of native speakers');
+//     break;
+//   case 'english':
+//     console.log('3rd place');
+//     break;
+//   case 'hindi':
+//     console.log('Number 4');
+//     break;  
+//   case 'arabic':
+//     console.log('5th most spoken language');
+//     break;  
+//   default:
+//     console.log('Great language too :D');
+// }
+
+const bill = 275;
+
+const tipDiscount = bill >= 50 && bill <= 300 ? 0.15 : 0.2;
+const tipValue = bill * tipDiscount; 
+
+console.log(`The bill was ${bill}, the tip was ${tipValue}, and the total value ${bill + tipValue}`);
 
 
