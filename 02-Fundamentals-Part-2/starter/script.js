a17ae5df6b83bf2b4bb21a596ee91d0f293198ce
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-18 12:50:03 +0700

    complete assignment

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,3 +9,16 @@ const finland = describeCountry('Finland', 4, 'Helsinki');
 const latvia = describeCountry('Latvia', 10, 'Riga');
 
 console.log(spain, finland, latvia);
+
+function percentageOfWorld1 (population) {
+  return population / 7900 * 100;
+}
+
+console.log(percentageOfWorld1(100));
+
+const percentageOfWorld2 = function (population) {
+  return population / 7900 * 100;
+}
+
+const res = percentageOfWorld2(7900);
+console.log(res);
