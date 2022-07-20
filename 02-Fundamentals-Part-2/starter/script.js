be3250e7b4e54b821e3df4a5c2ec961474dbe27f
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-20 16:24:17 +0700

    complete assignment

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -131,35 +131,47 @@ function percentageOfWorld1 (population) {
 // myCountry.checkIsland();
 // console.log(myCountry)
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI; 
-  }
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI; 
+//   }
+// }
+
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI; 
+//   }
+// }
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// if (mark.BMI > john.BMI) {
+//   console.log(`Mark's BMI (${mark.BMI})) is higher than John's (${john.BMI})!"`);
+// } else if (mark.BMI < john.BMI) {
+//   console.log(`John's BMI (${john.BMI}) is higher than Mark's (${mark.BMI})!`);
+// } else {
+//   console.log('Draw');
+// }
+
+// for (let i = 1; i <= 50; i++) {
+//   console.log (`Voter number ${i} is currently voting`);
+// }
+
+const populations = [10, 45, 23, 56];
+
+const percentages2 = [];
+
+for (let i = 0; i < populations.length; i++ ) {
+  percentages2.push(percentageOfWorld1(populations[i]));
 }
 
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI; 
-  }
-}
-
-mark.calcBMI();
-john.calcBMI();
-
-if (mark.BMI > john.BMI) {
-  console.log(`Mark's BMI (${mark.BMI})) is higher than John's (${john.BMI})!"`);
-} else if (mark.BMI < john.BMI) {
-  console.log(`John's BMI (${john.BMI}) is higher than Mark's (${mark.BMI})!`);
-} else {
-  console.log('Draw');
-}
-
-console.log()
+console.log(percentages2);
