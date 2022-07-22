010574841f962f68f958803550126025f4e28a80
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-22 10:58:49 +0700

    complete lesson

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,17 @@
 'use strict';
 
-document.querySelector('.message').textContent = 'Correct number!';
+// document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number!';
+  }
+});
