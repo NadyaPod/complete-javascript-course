6d8312ddccbc9a3fa4037c2cba929d62f166af3c
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-24 13:54:25 +0700

    implement lesson

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,6 +11,8 @@ let secretNumber = Math.trunc(Math.random() * 21);
 
 let score = 20;
 
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
@@ -23,6 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '↗ Too high!';
