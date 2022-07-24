224d7464f0be185873de4ddb013ff67e60daa65a
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-07-24 14:22:16 +0700

    complete project#1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,14 +13,17 @@ let score = 20;
 
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '❌ No number!';
+    displayMessage('❌ No number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✅ Correct number!';
+    displayMessage('✅ Correct number!');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -30,22 +33,13 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '↗ Too high!';
+      displayMessage(guess > secretNumber ? '↗ Too high!' : '↙ Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💔 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '↙ Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💔 You lost the game!';
+      displayMessage('💔 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -60,7 +54,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.number').textContent = '?';
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
 
   document.querySelector('.guess').value = '';
 
