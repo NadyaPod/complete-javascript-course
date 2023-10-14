749555e6351cec7fd81b13af17b90bb6670b27ee
Nadya Pod <nadegda.podyacheva@gmail.com>
2023-10-14 13:42:37 +0700

    complete cc#4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -157,3 +157,50 @@ for (let [key, value] of gameEvents) {
     console.log(`[SECOND HALF] ${value}`)
   }
 }
+
+// / Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const output = document.querySelector('textarea').value;
+  const pattern = '✅';
+  const outputArr = output.split('\n');
+  let counter = 1;
+
+  for (let word of outputArr) {
+    const [first, second] = word.trim().toLowerCase().split('_');
+    const secondUpper = second.replace(second[0], second[0].toUpperCase());
+    const resStr = `${first}${secondUpper}`
+    console.log(resStr.padEnd(20, ' ') + pattern.repeat(counter));
+    counter++;
+  }
+});
